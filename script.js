d5c517b499ce25c96870dcfe5b09f9a8f415bd36
Daniel Kurtz <daniel.kurtz@fau.de>
2020-12-02 14:42:07 +0100

    -

--- script.js
+++ script.js
@@ -176,21 +176,15 @@ $(document).ready(function(e) {
       var minutes = d.getMinutes();
       var seconds = d.getSeconds();
       var weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-      if (hours < 10) {
-         hours = "0" + hours;
-      }
-      if (minutes < 10) {
-         minutes = "0" + minutes;
-      }
-      if (seconds < 10) {
-         seconds = "0" + seconds;
-      }
+      if (hours < 10) hours = "0" + hours;
+      if (minutes < 10) minutes = "0" + minutes;
+      if (seconds < 10) seconds = "0" + seconds;
       var temptimestring = "[" + hours + ":" + minutes + ":" + seconds + "]";
       var tempdatestring = "";
       var ending = hours >= 12 ? 'PM' : 'AM';
       if (hours >= 12)  hours -= 12;
       if (hours==0) hours = 12;
+      if (hours < 10) hours = "0" + hours;
       var tempdatestring = weekdays[d.getDay()] + " " + hours + ":" + minutes + " " + ending;
      
 	  if (temptimestring != timestring) {
